ae9d4063e7ed602409f733b2505ec4d81c4ef9ad
Tamal <ckbtamaldipnew@gmail.com>
2026-03-06 21:37:45 +0600

    Got the understanding of controller

--- Backend/Controllers/TodoController.cs
+++ Backend/Controllers/TodoController.cs
@@ -4,22 +4,24 @@ using Backend.Services;
 
 namespace Backend.Controllers;
 
-[ApiController]
-[Route("api/todos")]
+[ApiController] // automatically handle things like valid json
+[Route("api/todos")] // base route
 public class TodoController : ControllerBase
 {
     private readonly TodoService _service;
 
     public TodoController(TodoService service)
     {
-        _service = service;
+        _service = service; //? Here it holds all the TodoService
     }
+    //? This Todo controller acts like import in ts. Like importing the controller or service in case of cs.
 
     [HttpGet]
     public IActionResult GetTodos()
     {
         return Ok(_service.GetAll());
     }
+    //? IActionResult means sending something like res.send(). Here we return Ok(), NotFound(), Created()
 
     [HttpPost]
     public IActionResult CreateTodo([FromBody] Todo todo)
